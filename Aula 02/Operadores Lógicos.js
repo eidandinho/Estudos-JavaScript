a8a9d3ff13d64cc0364dd896fac3743683a44eba
Evandro Lima <49327632+eidandinho@users.noreply.github.com>
2022-04-06 19:07:09 -0300

    Adição Operadores Lógicos - Aula 02

---
+++ Aula 02/Operadores Lógicos.js
@@ -0,0 +1,36 @@
+// &&  (AND) - Combina dois valores e retorna um true ou false. Ele só vai retornar true, quando os dois valores forem verdadeiros.
+
+var x = 3;
+var y = 7;
+
+x === 3 && y === 7;
+console.log(x === 3 && y === 7); //teste
+
+x === 3 && y === 4; //
+console.log(x === 3 && y === 4); //teste
+
+// || (OR) - Um único valor verdadeiro.
+
+x === 3 || y === 3; 
+console.log(x === 3 || y === 3); //teste
+
+// ! (NOT) - Inverte valores. Não muito usado, pode-se usar como x != 3;
+
+! x === 3;
+console.log(! x === 3); //teste
+
+x != 3;
+console.log(x != 3); //teste
+
+x !== 3;
+console.log(x != 3); //teste
+
+x != '3'; //string - X é igual a 3 string
+console.log(x != '3'); //teste
+
+x !== '3'; //string
+console.log(x !== '3'); //teste
+
+x !== 3; //
+console.log(x !== 3); //teste
+
