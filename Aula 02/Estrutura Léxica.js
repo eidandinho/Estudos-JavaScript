ae5c2553041d9db899ca675727d3b920a0e28e9b
Evandro Lima <49327632+eidandinho@users.noreply.github.com>
2022-04-06 20:05:13 -0300

    Anotações Estrutura Léxica

---
+++ Aula 02/Estrutura Léxica.js
@@ -0,0 +1,27 @@
+// Conjunto de regras que define os modos que se escrevem na linguagem.
+
+// Case sensitive - diferencia letras maiúsculas de minúsculas
+
+var animal = 'cachorro';
+console.log(animal); //teste
+
+var Animal = 'macaco';
+console.log(Animal); //teste
+
+console.log(animal !== Animal); // São duas variáveis diferentes
+
+// --------------- Comentários
+// Comentários  de Linha = //
+// Comentários de bloco = /* */
+
+
+/*  IDENTIFICADORES
+
+    Nomeia variáveis ou funções
+    Inicia com:
+    _ ou $
+    letras de "a" a "z"
+    letras de "A" a "Z"
+    digitos de 0 a 9
+    Qualquer caractere UNICODE (MAS NÃO DEVE-SE UTILIZAR)
+*/
